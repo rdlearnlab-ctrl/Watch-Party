46e54269cfaad0b78898b13175550566361b18ba
rdlearnlab-ctrl <rdlearnlab@gmail.com>
2026-08-22 00:13:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,12 @@ io.on('connection', (socket) => {
     socket.on('clear_board', (roomId) => socket.to(roomId).emit('receive_clear_board'));
     socket.on('sync_timer', (data) => socket.to(data.roomId).emit('receive_sync_timer', data));
     
+    // Board UI Sync
     socket.on('toggle_board', (data) => socket.to(data.roomId).emit('receive_toggle_board', data.isOpen));
+
+    // Hardware State Sync (Mute / Cam off)
+    socket.on('toggle_mute', (data) => socket.to(data.roomId).emit('peer_muted', data));
+    socket.on('toggle_cam', (data) => socket.to(data.roomId).emit('peer_cam_toggled', data));
 });
 
 const PORT = process.env.PORT || 3000;
