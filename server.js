f790f07c5ca09c70cbd53936d282234073eed5fd
rdlearnlab-ctrl <rdlearnlab@gmail.com>
2026-08-21 23:38:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-const activeRooms = {}; // Format: { roomId: { isPublic: boolean, users: number, hostSocketId: string } }
+const activeRooms = {}; // Format: { roomId: { isPublic: boolean, users: number, category: string, hostSocketId: string } }
 
 function getPublicRooms() {
     const publicRooms = [];
     for (const roomId in activeRooms) {
         if (activeRooms[roomId].isPublic) {
-            publicRooms.push({ roomId, users: activeRooms[roomId].users });
+            publicRooms.push({ 
+                roomId, 
+                users: activeRooms[roomId].users, 
+                category: activeRooms[roomId].category || 'Movie Night' 
+            });
         }
     }
     return publicRooms;
@@ -25,7 +29,8 @@ io.on('connection', (socket) => {
             activeRooms[data.roomId] = { 
                 isPublic: data.isPublic, 
                 users: 0, 
-                hostSocketId: socket.id // First person to create is the host
+                category: data.category || 'Movie Night',
+                hostSocketId: socket.id 
             };
         }
         io.emit('update_rooms', getPublicRooms());
@@ -36,24 +41,15 @@ io.on('connection', (socket) => {
         socket.roomId = data.roomId;
         
         if (!activeRooms[data.roomId]) {
-            activeRooms[data.roomId] = { isPublic: false, users: 0, hostSocketId: socket.id };
+            activeRooms[data.roomId] = { isPublic: false, users: 0, category: 'Movie Night', hostSocketId: socket.id };
         }
         activeRooms[data.roomId].users++;
         
-        // Tell the user if they are the host
         const isHost = (activeRooms[data.roomId].hostSocketId === socket.id);
         socket.emit('set_host_status', isHost);
 
         io.emit('update_rooms', getPublicRooms());
         socket.to(data.roomId).emit('user_connected', data.peerId);
-    });
-
-    // Host Power: Transfer Host Control
-    socket.on('transfer_host', (data) => {
-        if (activeRooms[data.roomId] && activeRooms[data.roomId].hostSocketId === socket.id) {
-            // Find target socket by name/peer or broadcast a custom event
-            io.to(data.roomId).emit('notify_host_change', data.newHostName);
-        }
     });
 
     socket.on('disconnect', () => {
@@ -66,7 +62,7 @@ io.on('connection', (socket) => {
         }
     });
 
-    // Video Sync (Restricted to Host checks on frontend)
+    // Video Sync
     socket.on('play_video', (roomId) => socket.to(roomId).emit('receive_play'));
     socket.on('pause_video', (roomId) => socket.to(roomId).emit('receive_pause'));
     socket.on('seek_video', (data) => socket.to(data.roomId).emit('receive_seek', data.time));
